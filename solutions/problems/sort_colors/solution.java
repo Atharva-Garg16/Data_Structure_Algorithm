21379ae384c0f47da025bc7a89fc0c04967e4e93
Atharva-Garg16 <atharvagarg1610@gmail.com>
2025-11-20 19:01:17 +0000

    Sync LeetCode submission - Sort Colors (java)

---
+++ solutions/problems/sort_colors/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortColors(int[] a) {
+        int ones=0,zeroes=0,twos=0;
+       for(int i=0; i<a.length ;i++){
+        if(a[i]==0) zeroes++;
+        else if(a[i]==1)ones++;
+        else twos++;
+       }
+       for(int i=0; i<a.length;i++ ){
+        if(i>=0 && i<zeroes) a[i]=0;
+        else if(i>=zeroes && i<zeroes+ones)a[i]=1;
+        else a[i]=2;
+       }
+    }
+}
