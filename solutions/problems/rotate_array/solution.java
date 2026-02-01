02197eea584b37b386c880d8010325b7d6ffeb9f
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-02-01 16:32:31 +0000

    Sync LeetCode submission - Rotate Array (java)

---
+++ solutions/problems/rotate_array/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int []arr=new int[nums.length];
+        
+        int n =nums.length;
+        k=k%n;
+        int j=0;
+        for(int i=n-k; i<n;i++){
+         arr[j]=nums[i];
+         j++;
+        }
+        for(int i=0; i<n-k; i++){
+            arr[j]=nums[i];
+            j++;
+        }
+        for(int i=0; i<n;i++){
+            nums[i]=arr[i];
+        }
+    }
+}
