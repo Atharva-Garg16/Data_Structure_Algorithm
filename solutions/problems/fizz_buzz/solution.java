f87c6e77b0d20ce41903e84d73fba939370ca982
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-02-06 01:27:07 +0000

    Sync LeetCode submission - Fizz Buzz (java)

---
+++ solutions/problems/fizz_buzz/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String>al=new ArrayList<>();
+                for(int i=1; i<=n;i++){
+            if(i%3==0 && i%5==0){
+                    al.add(("FizzBuzz"));
+                }
+                 else if(i%5==0){
+                    al.add(("Buzz"));
+                }
+                 else if(i%3==0){
+                    al.add("Fizz");
+                  }
+                  else{
+                    al.add(""+i);
+                  }
+        }
+        return al;
+        
+    }
+}
