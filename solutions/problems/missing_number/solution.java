05034b8db054968becf6fc41d20aa17dbf300eab
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-02-24 16:39:27 +0000

    Sync LeetCode submission - Missing Number (java)

--- solutions/problems/missing_number/solution.java
+++ solutions/problems/missing_number/solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-       Arrays.sort(nums);
-       for(int i=0; i<nums.length;i++){
-        if(nums[i]!=i) return i;
-       }
-       return nums.length;
+         int n=nums.length, ctr=0;
+        for(int i=0; i<n; i++){
+            ctr+=nums[i];
+        }
+        return ((n*(n+1))/2)-ctr;
     }
 }
