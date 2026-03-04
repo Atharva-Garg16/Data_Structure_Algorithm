d1ba9cc6535e41c3ddddb029be8870f175c12eed
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-03-04 11:45:45 +0000

    Sync LeetCode submission - Valid Palindrome (java)

--- solutions/problems/valid_palindrome/solution.java
+++ solutions/problems/valid_palindrome/solution.java
@@ -1,15 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String k=""; String res="";
+        StringBuilder res=new StringBuilder("");
         for(int i=0; i<s.length();i++){
             char ch= s.charAt(i);
             if((ch>=65 && ch<=90)|| (ch>=97&& ch<=122) || (ch>=48&& ch<=57)){
-                res=res+ch;
-                k=ch+k;
-
+               if(ch>=65 && ch<=90){
+                ch+=32;
+               }
+                res.append(ch);
             }
         }
-       
-        return  k.equalsIgnoreCase(res);
+       int left=0; int right= res.length()-1;
+    //    System.out.print(res);
+        while(left<=right){
+           if(res.charAt(left)!=res.charAt(right)){return false;}
+           left++; right--;
+        }
+        return true;
 }
 }
