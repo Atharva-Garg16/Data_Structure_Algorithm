927e43a7f30fe1e81ea755c70b88028550060a8d
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-03-09 02:48:11 +0000

    Sync LeetCode submission - N-th Tribonacci Number (java)

---
+++ solutions/problems/n-th_tribonacci_number/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int tribonacci(int n) {
+        int a=1,b=1,c=0,sum=0;
+      if(n==0) return 0;
+      if(n==1 || n==2 ) return 1;
+      for(int i=3; i<=n; i++){
+        sum=a+b+c;
+        c=b; b=a; a=sum;
+      } return sum;
+    }
+}
