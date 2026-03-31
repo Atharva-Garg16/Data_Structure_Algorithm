03aa0bb30946d94d5d146c13f0331c2566e8db97
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-03-31 03:34:18 +0000

    Sync LeetCode submission - Count Elements With Strictly Smaller and Greater Elements  (java)

---
+++ solutions/problems/count_elements_with_strictly_smaller_and_greater_elements_/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countElements(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != max && nums[i] != min) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
