949007145cda23f35470ff3f3fc1eaa18317aa8f
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-04-12 12:00:23 +0000

    Sync LeetCode submission - Replace Elements with Greatest Element on Right Side (java)

--- solutions/problems/replace_elements_with_greatest_element_on_right_side/solution.java
+++ solutions/problems/replace_elements_with_greatest_element_on_right_side/solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int k=arr.length;
-        int[] arr2=new int[k];
-        arr2[k-1]=-1;
-        for(int i=0;i<k-1;i++){
-            int max=-1;
-            for(int j=i+1;j<k;j++){
-             if(arr[j]>max){
-                max=arr[j];
-             }
-
-            }
-            arr2[i]=max;
-        } return arr2;
+        int max=arr[arr.length-1];
+        arr[arr.length-1]=-1;
+        for(int i=arr.length-2;i>=0;i--){
+            int prev=arr[i];
+           arr[i]=max;
+           if(max<prev){
+            max=prev;
+           }
+        } return arr;
     }
 }
