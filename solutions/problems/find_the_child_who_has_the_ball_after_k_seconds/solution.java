935b9d0c13ec6d4f2776a54515e9e4c14713d395
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-07-06 01:29:15 +0000

    Sync LeetCode submission - Find the Child Who Has the Ball After K Seconds (java)

---
+++ solutions/problems/find_the_child_who_has_the_ball_after_k_seconds/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfChild(int n, int k) {
+        int child=0,d=1;
+        for(int i=0;i<k;i++){
+            child+=d;
+            if(child==0){
+                d=1;
+            }
+            else if (child==n-1){
+                d=-1;
+            }
+        }
+       return child;
+        
+    }
+}
